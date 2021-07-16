d1dd6ec5aabaa915f2258256ac8f6bfc76a0892f
David Daza <03dazal@gmail.com>
2021-07-16 11:52:34 -0500

    config visualization page

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+function main() {
+  const iframe = document.querySelector('iframe')
+  const resource = document.getElementById('resource')
+  const search = window.location.search
+  
+  if (!search) return
+  
+  const dataURL = search.match(/data=(.*)&/)
+  const resourceURL = search.match(/resource=(.*)&/)
+  const originURL = search.match(/origin=(.*)&?/)
+
+  iframe.setAttribute('src', `https://datasketch.shinyapps.io/app-simple-charts-orgs/?org_name=ecuador-datos-abiertos&data_url=${dataURL[1]}
+  `)
+  resource.href = `${originURL[1]}${resourceURL[1]}`
+}
+
+window.addEventListener('DOMContentLoaded', main)
---
+++ content/visualiza.md
@@ -0,0 +1,4 @@
+---
+title: Visualiza
+layout: app
+---
