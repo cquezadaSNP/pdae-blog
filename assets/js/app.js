10aadf3f5a7ef2cd0b99560abbd48ef56a06498f
David Daza <03dazal@gmail.com>
2021-07-21 12:45:26 -0500

    add lang param to iframe

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ function main() {
   const resourceURL = search.match(/resource=(.*)&/)
   const originURL = search.match(/origin=(.*)&?/)
 
-  iframe.setAttribute('src', `https://datasketch.shinyapps.io/app-simple-charts-orgs/?org_name=ecuador-datos-abiertos&data_url=${dataURL[1]}
+  iframe.setAttribute('src', `https://datasketch.shinyapps.io/app-simple-charts-orgs/?lang=es&org_name=ecuador-datos-abiertos&data_url=${dataURL[1]}
   `)
   resource.href = `${originURL[1]}${resourceURL[1]}`
 }
